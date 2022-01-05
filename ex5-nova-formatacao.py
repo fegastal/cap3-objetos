70fe09386f90a974fab61a6306308c662b94fa9b
Fernanda Gastal <54810545+fegastal@users.noreply.github.com>
2022-01-05 18:51:15 +0000

    Add files via upload

---
+++ ex5-nova-formatacao.py
@@ -0,0 +1,8 @@
+num_3 = 1.0
+num_4 = 3.0
+
+mensagem_2 = "A divisão de %f por %f é igual a %f" % (num_3, num_4, num_3 / num_4)
+print(mensagem_2)
+
+mensagem_3 = "A divisão de %0.f por %0.f é igual a %.2f" % (num_3, num_4, num_3 / num_4)
+print(mensagem_3)
